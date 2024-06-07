326ea98656b5060cc23a44129a2201ba2a8cf20d
Maxim.Kabaev <maksim.kabaev@yandex.ru>
2024-06-07 23:15:36 +0300

    Fix file

--- intro/Collections.py
+++ intro/Collections.py
@@ -24,7 +24,6 @@ ids = {'user1': [213, 213, 213, 15, 213],
        'user2': [54, 54, 119, 119, 119],
        'user3': [213, 98, 98, 35]}
 
-
 new_ids = set()
 for value in ids.values():
     for elem in value:
