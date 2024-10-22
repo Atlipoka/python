7664803eabb904fbb3a8c9fa9daf18ffd3484299
Maxim.Kabaev <maksim.kabaev@yandex.ru>
2024-10-22 16:39:50 +0300

    Testis function super(). I guess it was last attempts.

--- intro/Encapsulation.inheritance.polymorphism.py
+++ intro/Encapsulation.inheritance.polymorphism.py
@@ -18,6 +18,24 @@ class Animal:
         else:
             print(f'Кормить животное {self.name} не нужно')
 
+    def songs(self):
+        if self.name == 'Гусь':
+            print('гага')
+        elif self.name == 'Корова':
+            print('муму')
+        elif self.name == 'Коза':
+            print('бебе')
+        else:
+            print('Невозможно определить голос животного, вид живонтного не указано или не добавлен в справочник')
+
+    def animal_features(self):
+        if self.name == 'Гусь':
+            print('Несет яйца')
+        elif self.name == 'Корова' or 'Коза':
+            print('Дает молоко')
+        else:
+            print('Невозможно определить особенности животного, вид живонтного не указано или не добавлен в справочник')
+
 class Goose(Animal):
     def __init__(self, name, voice, weight, features):
         self.name = name
@@ -25,16 +43,22 @@ class Goose(Animal):
         self.weight = weight
         self.features = features
 
-    def have_eggs(self):
-        val = input(f'Нужно ли собирать яйца у гуся {self.name}, напишите Да или Нет: ')
-        if val == 'Да':
-            print(f'Яйца собраны у гуся {self.name}')
+    def goose_features(self):
+        if self.features == '':
+            super().animal_features()
         else:
-            print('Яйца собирать не нужно')
+            print(f'{self.features}')
 
-goose_test = Goose('Гусь', 'Га-га', 2, 'Несет яйца')
-# print(dir(goose_test), goose_test.__dict__)
+    def goose_song(self):
+        if self.voice == '':
+            super().songs()
+        else:
+            print(f'{self.voice}')
 
+goose_test = Goose('Гусь', '', 2, '')
+print(dir(goose_test), goose_test.__dict__)
+# Goose.animal_features(self='Корова')
+goose_test.goose_features()
 # for method in dir(goose_test):
 #     print(method)
 
@@ -85,8 +109,7 @@ violator = Album('Violator', 'Depeche Mode', violator_tracks)
 # Проверям методы класса
 # violator.add_track(Track('Оblivion', 5))
 # print(violator_tracks)
-print(violator)
-print(track1 > track3)
+# print(violator)
+# print(track1 > track3)
 # violator.get_duration()
-###end###
 
